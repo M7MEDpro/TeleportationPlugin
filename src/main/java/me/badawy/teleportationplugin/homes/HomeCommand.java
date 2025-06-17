a6ee8be925649f9083ff166f5636663ba7252262
M7MEDpro <bdwym2007@gmail.com>
2025-06-17 07:51:58 +0300

    /home /sethome /remehome the whole home is done

--- src/main/java/me/badawy/teleportationplugin/homes/HomeCommand.java
+++ src/main/java/me/badawy/teleportationplugin/homes/HomeCommand.java
@@ -3,19 +3,55 @@ package me.badawy.teleportationplugin.homes;
 import dev.velix.imperat.BukkitSource;
 import dev.velix.imperat.annotations.Command;
 import dev.velix.imperat.annotations.Named;
-import dev.velix.imperat.annotations.SubCommand;
+import me.badawy.teleportationplugin.TeleportationPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
-@Command("home")
 public class HomeCommand {
+    private HomeDBManger homeDBManger;
+    private KeysHome keysHome;
+    @Command("sethome")
+    public void sethome(BukkitSource source, @Named("homename") String homename) {
 
-    @SubCommand("create")
-   public void CreateHome(BukkitSource source, @Named("homename") String homename) {
-        UUID uuid = source.asPlayer().getUniqueId();
-        if(HomeConfigManger.homesperplayer.get(uuid) >= HomeConfigManger.max_homes) {
-
+        if (!(source instanceof Player)) {
+            source.reply("You must be a player to use this command!");
             return;
         }
+
+            UUID uuid = source.asPlayer().getUniqueId();
+        if(HomeConfigManger.homesperplayer.get(uuid) >= HomeConfigManger.maxHomes) {
+            source.reply(HomeConfigManger.maxHomeMessage);
+            return;
+        }
+
+        homeDBManger.CreateHomeDataBase(source.asPlayer(),homename);
+    }
+    @Command("home")
+    public void home(BukkitSource source, String homename) {
+        if (!(source instanceof Player)) {
+            source.reply("You must be a player to use this command!");
+            return;
+        }
+        UUID uuid = source.asPlayer().getUniqueId();
+        String homeKey = keysHome.maphomekey(uuid, homename);
+        Bukkit.getScheduler().runTaskAsynchronously(TeleportationPlugin.getInstance(), () -> {
+           HomeObject homeObject = HomeDBManger.playerhomes.get(homeKey);
+           if (homeObject == null) {
+               source.reply(HomeConfigManger.noHomeMessage);
+               return;
+           }
+           ((Player) source).teleport(homeObject.getLocation());
+           source.reply(HomeConfigManger.teleportHomeMessage);
+        });
+    }
+    @Command("rmHome")
+    public void removeHome(BukkitSource source, String homename) {
+        if (!(source instanceof Player)) {
+            source.reply("You must be a player to use this command!");
+        }
+        homeDBManger.deleteHomeDataBase(source.asPlayer(),homename);
+        source.reply(HomeConfigManger.deleteHomeMessage);
     }
 }
--- src/main/java/me/badawy/teleportationplugin/homes/HomeConfigManger.java
+++ src/main/java/me/badawy/teleportationplugin/homes/HomeConfigManger.java
@@ -1,6 +1,7 @@
 package me.badawy.teleportationplugin.homes;
 
 import me.badawy.teleportationplugin.TeleportationPlugin;
+import org.bukkit.ChatColor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +9,11 @@ import java.util.UUID;
 
 public class HomeConfigManger {
     public static Map<UUID,Integer> homesperplayer = new HashMap<UUID,Integer>();
-    public static  int max_homes = TeleportationPlugin.getInstance().getConfig().getInt("max-homes");
 
+    public static int maxHomes = TeleportationPlugin.getInstance().getConfig().getInt("home.max-homes");
+    public static String setHomeMessage = ChatColor.translateAlternateColorCodes('&', TeleportationPlugin.getInstance().getConfig().getString("home.messages.set-home"));
+    public static String maxHomeMessage = ChatColor.translateAlternateColorCodes('&', TeleportationPlugin.getInstance().getConfig().getString("home.messages.max-home"));
+    public static String teleportHomeMessage = ChatColor.translateAlternateColorCodes('&', TeleportationPlugin.getInstance().getConfig().getString("home.messages.teleport-home"));
+    public static String noHomeMessage = ChatColor.translateAlternateColorCodes('&', TeleportationPlugin.getInstance().getConfig().getString("home.messages.no-home"));
+    public static String deleteHomeMessage = ChatColor.translateAlternateColorCodes('&', TeleportationPlugin.getInstance().getConfig().getString("home.messages.delete-home"));
 }
